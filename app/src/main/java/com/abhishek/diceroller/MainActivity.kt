8fbfb0d27e2f63426831ce91717161e4a3e3d1a5
Abhishek D S <abhishekds94@gmail.com>
2020-05-19 00:51:12 -0500

    Added Dice Images based on random num generator

--- app/src/main/java/com/abhishek/diceroller/MainActivity.kt
+++ app/src/main/java/com/abhishek/diceroller/MainActivity.kt
@@ -3,7 +3,11 @@ package com.abhishek.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +17,21 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button);
         rollButton.setOnClickListener {
-            Toast.makeText(this,"Rolled", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val randomInt = Random.nextInt(6)+1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
